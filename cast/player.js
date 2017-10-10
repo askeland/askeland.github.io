8923ea752620100e9153cb00f1da56690a177114
Christian <chr.askeland@gmail.com>
2017-10-10 14:26:13 +0200

    test with no animation

--- cast/player.js
+++ cast/player.js
@@ -817,10 +817,10 @@ function modify_image(image_src){
      }
      else {
        $androidImage.on('load', function() {
-        $(this).animate({width: scaledAttr.width, height: scaledAttr.height}, 0).fadeIn(200);
+        $(this).animate({width: scaledAttr.width, height: scaledAttr.height}, 0).fadeIn(0);
        });
 
-       $androidImage.fadeOut(200, function() {
+       $androidImage.fadeOut(0, function() {
         $(this).attr('src', i.src)
        });
      }
